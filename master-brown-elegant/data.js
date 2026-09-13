38a114d8862af9faf8a918aeafc49f29804b6bc5
nikiiggy <40224118+nikiiggy@users.noreply.github.com>
2026-09-13 12:17:38 +0700

    Create data.js

---
+++ master-brown-elegant/data.js
@@ -0,0 +1,51 @@
+const DATA_UNDANGAN = {
+  foto_cover: "https://images.unsplash.com/photo-1519741497674-611481863552?w=800",
+  foto_hero: "https://images.unsplash.com/photo-1583939003579-730e3918a45a?w=800",
+  audio_file: "../assets/audio/001.ogg", // Atau URL mp3 online
+  
+  pria: {
+    nama_panggilan: "Romeo",
+    nama_lengkap: "Romeo Montague, S.T.",
+    orang_tua: "Putra dari Bapak Lord Montague & Ibu Lady Montague",
+    foto: "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=400",
+    instagram: "https://instagram.com"
+  },
+  wanita: {
+    nama_panggilan: "Juliet",
+    nama_lengkap: "Juliet Capulet, S.Ds.",
+    orang_tua: "Putri dari Bapak Lord Capulet & Ibu Lady Capulet",
+    foto: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?w=400",
+    instagram: "https://instagram.com"
+  },
+  quote: {
+    salam: "Assalamu’alaikum Warahmatullahi Wabarakatuh",
+    teks: "Dan di antara tanda-tanda (kebesaran)-Nya ialah Dia menciptakan pasangan-pasangan untukmu dari jenismu sendiri, agar kamu cenderung dan merasa tenteram kepadanya.",
+    sumber: "QS. Ar-Rum: 21"
+  },
+  akad: {
+    tanggal: "Sabtu, 24 Oktober 2026",
+    jam: "08.00 WIB - Selesai",
+    lokasi: "Masjid Agung Surabaya",
+    maps: "https://maps.google.com"
+  },
+  resepsi: {
+    tanggal_iso: "2026-10-24T11:00:00",
+    tanggal: "Sabtu, 24 Oktober 2026",
+    jam: "11.00 - 14.00 WIB",
+    lokasi: "Grand Ballroom Hotel Utama",
+    maps: "https://maps.google.com"
+  },
+  save_the_date_url: "https://calendar.google.com/calendar/render?action=TEMPLATE&text=Pernikahan+Romeo+%26+Juliet&dates=20261024T080000Z/20261024T140000Z",
+  galeri: [
+    "https://images.unsplash.com/photo-1511285560929-80b456fea0bc?w=600",
+    "https://images.unsplash.com/photo-1522673607200-164d1b6ce486?w=600",
+    "https://images.unsplash.com/photo-1519225421980-715cb0215aed?w=600",
+    "https://images.unsplash.com/photo-1465495976277-4387d4b0b4c6?w=600"
+  ],
+  video_embed: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+  rekening: [
+    { bank: "BCA", nomor: "1234567890", pemilik: "Romeo Montague" },
+    { bank: "Mandiri", nomor: "0987654321", pemilik: "Juliet Capulet" }
+  ],
+  no_whatsapp_rsvp: "6281234567890"
+};
